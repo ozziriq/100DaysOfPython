85d8851c4bd5f25fbcbc4e6224a095560f6a979f
Oskar Karnehall <oskar@karnehall.com>
2022-10-10 10:51:44 +0200

    gf

---
+++ 100DaysOfPython/day1.py
@@ -0,0 +1,4 @@
+print("Vad ska ditt metalband heta? bara svara på frågorna så ska du se ")
+stad = (input("Vilken stad eller by växte du upp i "))
+djur = (input("Vad är ditt favoritdjur? "))
+print("Du, ditt band kan ju heta " + stad + djur + " Eller vad säger du?")
